284c831b2d339640100f899552536770dad11072
Obafemi Olabayo _bakijazzman_ <obafemipro@outlook.com>
2023-06-15 11:13:12 -0700

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio>
+/**
+ * main - printing sizes of different data type
+ * Return: 0 Always Success
+ */
+int main(void)
+{
+	printf("Size of a char : %d byte(s)\n", sizeof(char));
+	printf("Size of a int : %d byte(s)\n", sizeof(int));
+	printf("Size of a long int : %d byte(s)\n", sizeof(long int));
+	printf("Size of along long int : %d byte(s)\n", sizeof(long long int));
+	printf("Size of a float : %d byte(s)\n", sizeof(float));
+	return (0);
+
+}
