6a395e1110c018f6f5aec2d5bde6610c267a083e
Obafemi Olabayo _bakijazzman_ <obafemipro@outlook.com>
2023-06-15 10:58:32 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - using prinf to give std output
+ *
+ * Return: 0 Succe
+ *
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
