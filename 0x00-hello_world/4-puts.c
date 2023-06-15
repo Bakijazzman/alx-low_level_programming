2b832cde4fd71d5be09e999a303b6e624a7a875a
Obafemi Olabayo _bakijazzman_ <obafemipro@outlook.com>
2023-06-15 10:53:13 -0700

    betty again

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
-*	main- prints out the string of character
+*	main - prints out the string of character
 *
-*	return- 0 Always Success
+*	Return: 0 Always Success
 */
-int main(void)
+int main(void)	/* prints out  predetermined value */
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	return (0);	/*Always return 0*/
 }
