12891678ba617f09262a35ccfe517ed39e20aeb5
Obafemi Olabayo _bakijazzman_ <obafemipro@outlook.com>
2023-06-16 14:54:17 -0700

    iso c90 fixed

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,12 +14,11 @@
 int main(void)
 {
 	int n;
+	int num;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	int num;
-
 	num = n % 10;
 	if ((num != 0) && (num < 6))
 	{
