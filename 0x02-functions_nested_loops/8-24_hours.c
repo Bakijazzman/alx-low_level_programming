257a989f9eeeb4d9c0533fa65afa4d6c5d3caa53
Obafemi Olabayo _bakijazzman_ <obafemipro@outlook.com>
2023-06-20 19:29:26 -0700

    hot fix

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,27 +1,23 @@
-#include "main.h"
+include "main.h"
 /**
- * jack_bauer - function that rints out every minute of the day
+ * jack_bauer - prints every minutes of the day
+ * Return : void
+ *
  */
 void jack_bauer(void)
 {
-	int i, j;
+        int n, j;
 
-	i = 0;
-	while (i < 24)
-	{
-		j = 0;
-		while (j < 60)
-		{
-			_putchar((i / 10) + '0');
-			_putchar((i % 10) + '0');
-			_putchar(':');
-			_putchar((j / 10) + '0');
-			_putchar((j % 10) + '0');
-			_putchar('\n');
-			j++;
-		}
-	}
-	i++;
-
-
+        for (j = 0; j <= 23; j++)
+        {
+                for (n = 0; n <= 59; n++)
+                {
+                        _putchar((j / 10) + '0');
+                        _putchar((j % 10)  + '0');
+                        _putchar(':');
+                        _putchar((n / 10) + '0');
+                        _putchar((n % 10) + '0');
+                        _putchar('\n');
+                }
+        }
 }
