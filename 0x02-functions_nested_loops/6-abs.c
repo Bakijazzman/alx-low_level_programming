c34af6ff7a1276998cd9ab1f942230f2c5c1d6dc
Obafemi Olabayo _bakijazzman_ <obafemipro@outlook.com>
2023-06-20 19:12:13 -0700

    absolute number verification

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - function that computes the absolute value of an integer.
+ * @number: The input number to check
+ * Return: answer
+ */
+
+int _abs(int number)
+{
+	int answer;
+
+	if (number < 0)
+	{
+		answer = number * (-1);
+		return (answer);
+	}
+	else
+		return (answer);
+}
