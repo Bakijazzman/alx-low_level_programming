29f74c68fae5a1714d1274860800d6655c1a12e0
Obafemi Olabayo _bakijazzman_ <obafemipro@outlook.com>
2023-06-20 19:35:02 -0700

    all fixed

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,18 +6,18 @@
  */
 void jack_bauer(void)
 {
-        int n, j;
+	int n, j;
 
-        for (j = 0; j <= 23; j++)
-        {
-                for (n = 0; n <= 59; n++)
-                {
-                        _putchar((j / 10) + '0');
-                        _putchar((j % 10)  + '0');
-                        _putchar(':');
-                        _putchar((n / 10) + '0');
-                        _putchar((n % 10) + '0');
-                        _putchar('\n');
-                }
-        }
+	for (j = 0; j <= 23; j++)
+	{
+		for (n = 0; n <= 59; n++)
+		{
+			_putchar((j / 10) + '0');
+			_putchar((j % 10)  + '0');
+			_putchar(':');
+			_putchar((n / 10) + '0');
+			_putchar((n % 10) + '0');
+			_putchar('\n');
+		}
+	}
 }
