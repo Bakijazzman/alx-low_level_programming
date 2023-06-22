4ac2e2f53038aab0b3166fc9bedcbe9220a71bf3
Obafemi Olabayo _bakijazzman_ <obafemipro@outlook.com>
2023-06-22 10:08:46 -0700

    putchar print

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,20 +1,26 @@
-#include "main.h"
+#include <stdio.h>
 /**
- * more_numbers - prints 10 times the numbers 
+ * more_numbers - prints 10 times the numbers
  * from 0 to 14
  * Return: nothing
  */
-void more_numbers(void)
+void main(void)
 {
-	int i, j;
+        int i, j;
 
-	for (i = 0; i < 10; i++)
-	{
-		for (j = 0; j < 14 ; j++)
-		{
-			_putchar(j + '0');
-			_putchar('\n');
-		}
-	}
-	_putchar('\n');
+        for (i = 0; i < 10; i++)
+        {
+                for (j = 0; j < 15 ; j++)
+                {
+                    if (j >= 10)
+                   /* {
+                         putchar(j + '0');
+                    }
+                    else
+                    {*/
+                        putchar(j / 10 + '0');
+                        putchar(j % 10 + '0');
+                }
+                putchar('\n');
+        }
 }
