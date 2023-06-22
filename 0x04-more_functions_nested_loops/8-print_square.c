0dd256f48ca08db668e56d3f7cca5aba5e9a5537
Obafemi Olabayo _bakijazzman_ <obafemipro@outlook.com>
2023-06-22 11:55:11 -0700

    print square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_square - function that print a square
+ * @size: size of the  square
+ * Return: nothing
+ */
+void print_square(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i, j;
+
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 1; j <= size; j++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+		}
+	}
+}
