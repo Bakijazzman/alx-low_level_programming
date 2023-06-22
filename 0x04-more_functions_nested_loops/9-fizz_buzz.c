ee901b751497e0b14dd2981040e33b645dc3f4f7
Obafemi Olabayo _bakijazzman_ <obafemipro@outlook.com>
2023-06-22 12:48:31 -0700

    buuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,6 +14,10 @@ int main(void)
 		{
 			printf("FizzBuzz ");
 		}
+		else if (i == 100)
+		{
+			printf("Buzz");
+		}
 		else if (i % 5 == 0 && i % 3 != 0)
 		{
 			printf("Buzz ");
@@ -21,10 +25,6 @@ int main(void)
 		else if (i % 3 == 0 && i % 5 != 0)
 		{
 			printf("Fizz ");
-		}
-		else if (i == 100)
-		{
-			printf("Buzz");
 		}
 		else
 		{
