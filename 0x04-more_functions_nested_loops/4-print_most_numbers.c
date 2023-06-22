16b0189aa1f8f2a99ef533320eeac1b7eefe0e28
Obafemi Olabayo _bakijazzman_ <obafemipro@outlook.com>
2023-06-22 08:54:12 -0700

    print ost numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints number from 0-9 exluding 2 & 4
+ * Return: nothing
+ *//
+void print_most_numbers(void)
+{
+	int b;
+
+	b = 0;
+	while(b < 10)
+	{
+		if (b = 2 || b = 4)
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(b + '0');
+			b++;
+		}
+	}
+	_putchar('\n');
+}
