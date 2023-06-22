aedc23e20ee038bd45853066c3a92ba391c58514
Obafemi Olabayo _bakijazzman_ <obafemipro@outlook.com>
2023-06-22 08:35:54 -0700

    still not compiling

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * print_numbers - function that prints numbers from 0-9
  * Return: nothing
@@ -8,7 +9,7 @@ void print_numbers(void)
 	int n;
 
 	n = 0;
-	while (n < 10)
+	while (n <= 9)
 	{
 		_putchar(n + '0');
 		n++;
