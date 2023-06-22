2f695ad3423eccf172aa82486782c3d8725c2a68
Obafemi Olabayo _bakijazzman_ <obafemipro@outlook.com>
2023-06-22 06:33:19 -0700

    betty

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- *  * main - tests the function that prints if the integer is negative or positive
- *   * Return: 0
- *    */
+ * main - tests the function that prints if the integer is negative or positive
+ * Return: 0
+ */
 
 int main(void)
 {
