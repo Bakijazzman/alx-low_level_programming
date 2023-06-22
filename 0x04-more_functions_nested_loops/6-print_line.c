69969e0ef80b63d4c744493fea34fdb986d10aff
Obafemi Olabayo _bakijazzman_ <obafemipro@outlook.com>
2023-06-22 10:32:47 -0700

    print line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -6,18 +6,19 @@
  */
 void print_line(int n)
 {
-	int i;
-
 	if (n == 0)
 	{
 		_putchar('\n');
 	}
 	else
 	{
+		int i;
+
 		for (i = 0; i <= n; i++)
 		{
 			_putchar('_');
 		}
 	}
+	_putchar ('\n');
 
 }
