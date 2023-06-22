0df4fc736bc3409272d68bc027c20d9da30a009d
Obafemi Olabayo _bakijazzman_ <obafemipro@outlook.com>
2023-06-22 08:22:18 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include "stdio.h"
+/**
+ * print_numbers - function that prints numbers from 0-9
+ * Return: nothing
+ */
+void print_numbers(void)
+{
+	int n;
+
+	n = 0;
+	while (n < 10)
+	{
+		_putchar(n);
+		n++;
+	}
+}
