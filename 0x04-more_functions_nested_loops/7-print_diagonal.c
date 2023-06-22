79207f9e95e72cc806ac79cc5ee539f11b636446
Obafemi Olabayo _bakijazzman_ <obafemipro@outlook.com>
2023-06-22 11:22:22 -0700

    print diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * print-diagonal - function that draws a diagonal line 
+ * on the terminal
+ * @n: parameter parsed in
+ * Return: nothing
+ */
+void print_diagonal(int n)
+{
+	if (n == 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i, j;
+
+		for (i = 1; i <= n; i++)
+		{
+			for (j = 1; j <= n; j++)
+			{
+				if(j == i)
+					_putchar('\\');
+				else if (j < i)
+					_putchar(' ');
+			}
+			_putchar('\n');
+		}
+	}
+}
