17b7b41dcf8d31d9e887117c75e0a4cf061615e9
Obafemi Olabayo _bakijazzman_ <obafemipro@outlook.com>
2023-06-22 12:16:12 -0700

    fizz buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - fizz buzz challenge
+ * Return: 0 success
+ */
+int main(void)
+{
+	int i, j;
+
+	j = 100;
+	for (i = 1; i <= j; i++ )
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 5 == 0 && i % 3 != 0)
+		{
+			printf("Buzz ");
+		}
+		else if (i % 3 == 0 && i % 5 != 0)
+		{
+			printf("Fizz ");
+		}
+		else
+		{
+			printf("%i", i);
+		}
+	}
+
+	return (0);
+}
