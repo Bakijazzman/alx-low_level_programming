15b8733cfde133166e7ef9a22ee283d61e94964f
Obafemi Olabayo _bakijazzman_ <obafemipro@outlook.com>
2023-06-26 17:38:36 -0700

    issue fixed

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
  * swap_int - a function that swaps the values of two integers
- * @a: parameter 
+ * @a: parameter
  * @b: parameter
  */
 void swap_int(int *a, int *b)
 {
 	int new = *a;
-	*b = new;
-	*a = *b;
+
+	new = *b;
+	*b = *a;
 }
