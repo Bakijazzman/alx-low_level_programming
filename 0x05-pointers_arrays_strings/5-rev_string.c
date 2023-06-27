e38374c48dbcdbd683690a508c7621f5d0e6c7a8
Obafemi Olabayo _bakijazzman_ <obafemipro@outlook.com>
2023-06-26 18:12:28 -0700

    string reverse

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,2 +1,24 @@
 #include "main.h"
 /**
+ * rev_string - reverses a string
+ *
+ * @s: string parameter input
+ *
+ * Return: Nothing
+ */
+void rev_string(char *s)
+{
+	int total1, i;
+	char ch;
+
+	/*find string length without null char*/
+	for (total1  = 0; s[total1] != '\0'; total1++)
+		;
+
+	for (total2 = 0; total2 < l / 2; total2++)
+	{
+		ch = s[total2];
+		s[i] = s[total1 - 1 - total2];
+		s[total1 - 1 - total2] = ch;
+	}
+}
