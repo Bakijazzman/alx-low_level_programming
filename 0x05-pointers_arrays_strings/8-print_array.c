560fab623fa5293db91b887111471ba7f6b97478
Obafemi Olabayo _bakijazzman_ <obafemipro@outlook.com>
2023-06-26 19:44:26 -0700

    fixing

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,8 +11,11 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%d, ", a[i - 1]);
-		if (i == n - 1)
+		if (n != n - 1)
+		{
+			printf("%d, ", a[i - 1]);
+		}
+		else
 		{
 			printf("%d", a[n - 1]);
 		}
