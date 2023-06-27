237fb4117dcbadcce85747fa209e6e1baae62d81
Obafemi Olabayo _bakijazzman_ <obafemipro@outlook.com>
2023-06-26 19:36:46 -0700

    array display

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_array - prints elements of an array
+ * @a: the array to be printed
+ * @n: number of times
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d, ", a[i]);
+		if (i == n - 1)
+		{
+			printf("%d", a[n - 1]);
+		}
+
+	}
+	printf("\n");
+}
