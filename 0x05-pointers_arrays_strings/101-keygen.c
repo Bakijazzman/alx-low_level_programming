781a3fca37a69a8a758dc35bcd09992518d64f73
Obafemi Olabayo _bakijazzman_ <obafemipro@outlook.com>
2023-06-26 20:12:40 -0700

    keygen

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+#define MIN_LENGTH 8
+#define MAX_LENGTH 12
+
+char *generate_password(void)
+{
+	int i;
+	char *password = malloc(MAX_LENGTH + 1);
+	int password_length = rand() % (MAX_LENGTH - MIN_LENGTH + 1) + MIN_LENGTH;
+	const char charset[];
+
+	charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+	srand(time(NULL));
+
+	for (i = 0; i < password_length; i++)
+	{
+		int index = rand() % strlen(charset);
+
+		password[i] = charset[index];
+	}
+	password[password_length] = '\0';
+
+	return (password);
+}
+
+
+int main(void)
+{
+	char *password = generate_password();
+
+	printf("%s\n", password);
+	free(password);
+
+	return (0);
+}
