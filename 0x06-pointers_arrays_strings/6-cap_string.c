fd971cde018ff7ea0d7d46a7f3b601cce39d178f
Obafemi Olabayo _bakijazzman_ <obafemipro@outlook.com>
2023-06-29 16:27:20 -0700

    cap string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,45 @@
+#include "main.h"
+/**
+ * cap_string - function that capitalizes all words of a string
+ * Return: the char data type
+ */
+char *cap_string(char *)
+{
+	char ch;
+	int i = 0;
+	int cap_next = 1;
+
+	while (*(ch + i) != '\0')
+	{
+		if (cap_next && (*(ch + i) >= 'a' && *(ch + i) <= 'z'))
+		{
+			*(ch + i) = *(ch + i) - 'a' + 'A';
+		}
+		cap_next = 0;
+
+		switch (*(ch + i))
+		{
+			case ' ':
+			case '\t':
+			case '\n':
+			case ',':
+			case ';':
+			case '.':
+			case '!':
+			case '?':
+			case '"':
+			case '(':
+			case ')':
+			case '{':
+			case '}':
+			cap_next = 1;
+			break;
+			default:
+			break;
+		}
+
+		i++;
+	}
+
+		return (ch);
+}
