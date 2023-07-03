df29e0ea631281ec0413b8290606d0f94c34345b
Obafemi Olabayo _bakijazzman_ <obafemipro@outlook.com>
2023-07-03 13:45:18 -0700

    memory copy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _memcpy - function that copies memory area
+ * @dest: the memory area
+ * @src: the memory area
+ * @n: copies
+ * Return: dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	if (n > 0)
+	{
+		int num = 0;
+
+		while (num < n)
+		{
+			dest[i] = src[i];
+			num++;
+		}
+	}
+	return (dest);
+}
