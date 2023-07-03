260c08991d62e5ee43f5fd5742df737646ae9f70
Obafemi Olabayo _bakijazzman_ <obafemipro@outlook.com>
2023-07-03 14:11:56 -0700

    string check

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strchr - function that locates a string
+ * @s: the string to be checked
+ * @c: the character to  be checked for
+ * Return: pointer to the first occurence of the character
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+
+	while (*s != '\0' && s[i] != c)
+	{
+		i++;
+		if (s[i] == c)
+		{
+			return (&s[i]);
+		}
+		else
+		{
+			return (NULL);
+		}
+	}
+}
