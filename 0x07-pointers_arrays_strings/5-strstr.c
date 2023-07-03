7febe9e52107330bbbf5d0ccedea8e3b01907f64
Obafemi Olabayo _bakijazzman_ <obafemipro@outlook.com>
2023-07-03 15:00:36 -0700

    substring

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strstr - function that locates a substring
+ * @haystack: first string
+ * @neddle: second string
+ * Return: char
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	char *result;
+	char *aux;
+
+	while (*haystack != '\0')
+	{
+		result = haystack;
+		aux = needle;
+
+		while (*aux == *haystack && *aux != '\0'
+		       && *haystack != '\0')
+		{
+			haystack++;
+			aux++;
+		}
+		if (*aux == '\0')
+			return (result);
+		haystack = result + 1;
+	}
+	return (0);
+}
