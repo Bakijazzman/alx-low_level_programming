5d15648cb0bb47c29706448d7ad9f8aa6378599d
Obafemi Olabayo _bakijazzman_ <obafemipro@outlook.com>
2023-07-05 11:07:39 -0700

    recurssion puts

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _puts_recursion - prints a string followed by a new line
+ * @s: the string to be printed
+ */
+void _puts_recursion(char *s)
+{
+	int i = 0;
+
+	if (s[i] == 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	else
+	{
+		_putchar(s[i]);
+		i++;
+	}
+	_put_recursion(s);
+}
