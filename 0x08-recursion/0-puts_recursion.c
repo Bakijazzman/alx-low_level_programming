c6d47788ae80f2e6b2a6144dd25af04288741aef
Obafemi Olabayo _bakijazzman_ <obafemipro@outlook.com>
2023-07-05 11:13:18 -0700

    recurssion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,16 +5,14 @@
  */
 void _puts_recursion(char *s)
 {
-	int i = 0;
-
-	if (s[i] == '\0')
+	if (*s == '\0')
 	{
 		_putchar('\n');
 		return;
 	}
 	else
 	{
-		_putchar(s[i]);
+		_putchar(*s);
 		i++;
 	}
 	_puts_recursion(s);
