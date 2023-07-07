0fcd6aef701a6bcebbe9ece62801d15e657aaf19
Obafemi Olabayo _bakijazzman_ <obafemipro@outlook.com>
2023-07-07 11:52:21 -0700

    multipl

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - multiplies arguements recieved
+ * @argc: the counts of the arguements
+ * @argv: the value of the arguement
+ * Return: 1
+ */
+int main(int argc, char *argv[])
+{
+	int result, a, b;
+
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+	result = a * b;
+	if (argc !=  3)
+	{
+		printf("Error");
+		return (1);
+	}
+	else
+	{
+		printf("%d\n", result);
+	}
+	return (1);
+}
