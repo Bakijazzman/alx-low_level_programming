c42f0235e9d293c4de2d78b080490541d7b1b62b
Obafemi Olabayo _bakijazzman_ <obafemipro@outlook.com>
2023-07-07 10:34:48 -0700

    whats my name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
  * main - prints the name of a program
- * @agrc: counts each agurment
- * @agrv: prints each aguement
+ * @argc: counts each agurment
+ * @argv: prints each aguement
  * Return: 0 success
  */
-int main(int argc, char argv[])
+int main(int argc, char *argv[])
 {
-	printf("%s\n", agrv[0]);
+	printf("%s\n", argv[0]);
 	return (0);
 }
