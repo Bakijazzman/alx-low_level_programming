69694561c55d837169cba5e7b7cdb171e06346cb
Obafemi Olabayo _bakijazzman_ <obafemipro@outlook.com>
2023-07-07 12:04:43 -0700

    multiply

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,16 +10,16 @@ int main(int argc, char *argv[])
 {
 	int result, a, b;
 
-	a = atoi(argv[1]);
-	b = atoi(argv[2]);
-	result = a * b;
-	if (argc !=  3)
+	if (argc !=  3 && argc < 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
 	else
 	{
+		a = atoi(argv[1]);
+		b = atoi(argv[2]);
+		result = a * b;
 		printf("%i\n", result);
 	}
 	return (1);
