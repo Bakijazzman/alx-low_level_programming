b1a1d29938d4ab5179a931a760ba9ae925970186
Obafemi Olabayo _bakijazzman_ <obafemipro@outlook.com>
2023-07-17 08:20:23 -0700

    initialize dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * init_dog - variable of the structure
+ * @d: pointer to mydog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of owner
+ * Return: nothing
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		(*d).name = name;
+		(*d).age = age;
+		(*d).owner = owner;
+	}
+}
