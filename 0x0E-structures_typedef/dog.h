2f4349c543551956d3b24673279e787669f89181
Obafemi Olabayo _bakijazzman_ <obafemipro@outlook.com>
2023-07-17 11:03:07 -0700

    betty comppliant

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,7 @@
 #ifndef _DOG_H
 #define _DOG_H
 /**
- * strect dog - structure or object
+ * dog - structure or object
  * @name: name of the dog
  * @age: age of tyhe dog
  * @owner: owner of the dog
