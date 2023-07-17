bd3a1e6858d634fd25c9b51e294f2ead57ca2639
Obafemi Olabayo _bakijazzman_ <obafemipro@outlook.com>
2023-07-17 08:06:45 -0700

    structures

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef _DOG_H
+#define _DOG_H
+/**
+ * strect dog - structure or object
+ * @name: name of the dog
+ * @age: age of tyhe dog
+ * @owner: owner of the dog
+ */
+	struct dog
+	{
+		char * name;
+		float age;
+		char *owner;
+	};
+#endif
