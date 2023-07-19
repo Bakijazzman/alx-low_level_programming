d12b74f119ca28dbfe45320a9038c1cc3431fcb3
Obafemi Olabayo _bakijazzman_ <obafemipro@outlook.com>
2023-07-19 10:50:10 -0700

    array of functions

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * array_iterator - function that executes a function given as a parameter
+ * @array: the array of functions
+ * @size: the size if the array
+ * @action: the function to be executed
+ * Return: nothing
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (!array || !action)
+	{
+		return;
+	}
+	else
+	{
+		for (i = 0;i < size; i++)
+		{
+			action(array[i]);
+		}
+	}
+}
