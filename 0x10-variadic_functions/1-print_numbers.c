1adde1267b072e47b2acac6defb3b75dfdc2c0ec
Obafemi Olabayo _bakijazzman_ <obafemipro@outlook.com>
2023-07-20 15:14:51 -0700

    print

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,10 +13,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	va_start(list, n);
 
-	for (; i < n; i++)
+	for (i; i < n; i++)
 	{
 		printf("%d", va_arg(list, int));
-		if (separator != 0 && i < (n - 1))
+		if (separator != NULL && i < (n - 1))
 		{
 			printf("%s", separator);
 		}
