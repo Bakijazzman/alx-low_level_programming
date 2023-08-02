1babd16fb54f858bade2b247a44743aaee3dfe04
Bakijazzman <obafemiolabayo@gmail.com>
2023-08-01 20:00:41 -0700

    insert node in a location

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,51 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - inserts a new node in a give location.
+ * @head: head of a list.
+ * @idx: index of the list where the new node is
+ * added.
+ * @n: integer element.
+ *
+ * Return: the address of the new node, or NULL if it
+ * failed.
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	size_t i;
+	listint_t *create;
+	listint_t *c;
+
+	c = *head;
+
+	if (idx != 0)
+	{
+		for (i = 0; i < idx - 1 && h != NULL; i++)
+		{
+			c = c->next;
+		}
+	}
+
+	if (c == NULL && idx != 0)
+		return (NULL);
+
+	create = malloc(sizeof(listint_t));
+	if (create == NULL)
+		return (NULL);
+
+	create->n = n;
+
+	if (idx == 0)
+	{
+		create->next = *head;
+		*head = new;
+	}
+	else
+	{
+		create->next = c->next;
+		c->next = create;
+	}
+
+	return (create);
+}
