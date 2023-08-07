73b99066c8b9c366bea3195489218c9d19810781
Bakijazzman <obafemiolabayo@gmail.com>
2023-08-07 12:54:38 -0700

    create file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+* create_file - Function that creates a file
+* @filename: Name of the file to create
+* @text_content: A NULL terminated string to write to the file
+*
+* Return: 1 on success, -1 on failure
+**/
+
+int create_file(const char *filename, char *text_content)
+{
+	ssize_t wordCount;
+	int n = -1, i, desc;
+
+	if (!filename)
+		return (n);
+
+	desc = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (desc == n)
+		return (n);
+
+	if (text_content)
+		for (i = 0; text_content[i] != '\0'; i++)
+		{
+			wordCount = write(desc, text_content, i);
+			if (wordCount == n || wordCount != i)
+			{
+				close(desc);
+				return (n);
+			}
+		}
+
+	close(desc);
+	return (1);
+}
