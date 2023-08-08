e1f10d0dfc59186e91f3390735fdbfc075a33cfc
Bakijazzman <obafemiolabayo@gmail.com>
2023-08-08 03:11:40 -0700

    betty fix

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -2,16 +2,15 @@
 
 /**
  * list_len - Returns the number of elements in a linked list_t
- * @h: Head 
+ * @h: Head
  *
  * Return: Num of elements
  **/
 size_t list_len(const list_t *h)
 {
-	int i;
+	int node_count;
 
-	for (i = 0; h; i++, h = h->next)
+	for (node_count = 0; h; node_count++, h = h->next)
 		;
-
-	return (i);
+	return (node_count);
 }
