07024b5acba9dca644cf12f8e9298e7374c3462d
Bakijazzman <obafemiolabayo@gmail.com>
2023-10-06 10:57:03 +0100

    betty fix for first task

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,11 @@
+#include "hash_tables.h"
+/**
+ * hash_djb2 - implements djb2 algorithm
+ * @str: string to be parsed
+ * Return: value
+ */
+unsigned long int hash_djb2(const unsigned char *str)
+{
+
+	return (djb2);
+}
