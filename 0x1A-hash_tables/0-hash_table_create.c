42d20c33ceec27e87a4556eb083a10be0b99c31b
Bakijazzman <obafemiolabayo@gmail.com>
2023-10-06 10:09:17 +0100

    hash table init

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,12 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - Creats and initialize a hash_table
+ * size: size of the table
+ * Return: pointer to the tavle created or null if something went wrong
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+
+
+
+}
