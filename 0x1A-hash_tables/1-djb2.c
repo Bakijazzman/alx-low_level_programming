8dca22f03ca8728de79dc36aa277ff18a643ad33
Bakijazzman <obafemiolabayo@gmail.com>
2023-10-06 11:05:29 +0100

    djb2 algorith

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -6,6 +6,13 @@
  */
 unsigned long int hash_djb2(const unsigned char *str)
 {
+	unsigned long int djb2;
+	int i;
 
+	djb2 = 5381;
+	while ((i = *str++))
+	{
+		djb2 = ((djb2 << 5) + djb2) + i;
+	}
 	return (djb2);
 }
