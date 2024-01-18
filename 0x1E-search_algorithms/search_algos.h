0475dff845d24851fa633acf5982d67aeed61f41
Bakijazzman <obafemiolabayo@gmail.com>
2024-01-18 08:00:11 +0100

    advamced binary

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -14,5 +14,7 @@ int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int b_search(int *array, size_t low, size_t high, int value);
 int exponential_search(int *array, size_t size, int value);
-
+int binary_search_recursion(int *array, int value,
+			    size_t low, size_t high);
+int advanced_binary(int *array, size_t size, int value);
 #endif
